5e10e87649a1268187cef75885df6ebef264329f
Hoan Dao <101268839+DDH2808@users.noreply.github.com>
2024-04-05 22:54:28 +0700

    Update Coach.java

--- foooball_app/src/main/java/com/example/foooball_app/entity/Coach.java
+++ foooball_app/src/main/java/com/example/foooball_app/entity/Coach.java
@@ -1,21 +1,72 @@
 package com.example.foooball_app.entity;
 import java.util.Date;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
+import jakarta.persistence.*;
+
+@Entity
 @Table(name = "coachs")
 public class Coach {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int coachId;
+    private Long  coachId;
 
     private String fullName;
 
-    private Date dateOfBirth;
-
     private String country;
 
+    private Date dateOfBirth;
+
+    private  String team;
+
+    private Integer yearsOfExperience;
+
     // Getters and setters
+
+    public Long  getCoachId() {
+        return coachId;
+    }
+
+    public void setCoachId(Long coachId) {
+        this.coachId = coachId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public Integer getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(Integer yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
 }
