058807265461831710e5a9d4f38df4dc64205479
thehung <nguyenthehung22122002@gmail.com>
2024-04-05 18:05:54 +0700

    create player 2

--- foooball_app/src/main/java/com/example/foooball_app/controller/PlayerController.java
+++ foooball_app/src/main/java/com/example/foooball_app/controller/PlayerController.java
@@ -1,10 +1,8 @@
 package com.example.foooball_app.controller;
 
 import com.example.foooball_app.entity.Player;
-import com.example.foooball_app.request.ApiResponse;
 import com.example.foooball_app.request.PlayerCreateRequest;
 import com.example.foooball_app.service.PlayerService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +14,7 @@ public class PlayerController {
     private PlayerService playerService;
 
     @PostMapping("/players")
-    ApiResponse<Player> createPlayer(@RequestBody @Valid PlayerCreateRequest req){
-        ApiResponse<Player> apiResponse = new ApiResponse<>();
-        apiResponse.setResult(playerService.createPlayerService(req));
-        return apiResponse;
+    Player createPlayer(@RequestBody PlayerCreateRequest req){
+        return playerService.createPlayerService(req);
     }
 }
