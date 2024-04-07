28ae2bfdb736b01d0b23ef32b13f74009c9b3145
Hoan Dao <101268839+DDH2808@users.noreply.github.com>
2024-04-07 17:58:55 +0700

    Delete foooball_app/src/main/java/com/example/foooball_app/exception/TournamentNotFoundException.java

--- foooball_app/src/main/java/com/example/foooball_app/exception/TournamentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.foooball_app.exception;
-
-public class TournamentNotFoundException extends RuntimeException{
-    public TournamentNotFoundException(Long id) {
-        super("Could not find tournament with id " + id);
-    }
-}
