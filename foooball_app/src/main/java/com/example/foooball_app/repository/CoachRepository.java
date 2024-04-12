cc7a1415878f62cfb5b1d76383c771fca53e8684
Hoan Dao <101268839+DDH2808@users.noreply.github.com>
2024-04-12 09:27:05 +0700

    Update CoachRepository.java

--- foooball_app/src/main/java/com/example/foooball_app/repository/CoachRepository.java
+++ foooball_app/src/main/java/com/example/foooball_app/repository/CoachRepository.java
@@ -3,17 +3,12 @@ package com.example.foooball_app.repository;
 import com.example.foooball_app.entity.Coach;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
-public interface CoachRepository extends JpaRepository<Coach, Long> {
+public interface CoachRepository extends JpaRepository<Coach, Integer> {
 
     boolean existsByCoachName(String coachName);
     boolean existsByCountry(String country);
     boolean existsByYearsOfExperience(Integer yearsOfExperience);
 
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> af9d94641fd062ce08cf4020196d90695601d54c
