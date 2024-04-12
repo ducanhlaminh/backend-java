bfe95f609e7df3f50c1b9ef873fe120b7272dba3
Hoan Dao <101268839+DDH2808@users.noreply.github.com>
2024-04-12 09:23:50 +0700

    Update CoachRequest.java

--- foooball_app/src/main/java/com/example/foooball_app/dto/request/CoachRequest.java
+++ foooball_app/src/main/java/com/example/foooball_app/dto/request/CoachRequest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.foooball_app.dto.request;
 
 import java.sql.Date;
@@ -57,63 +56,3 @@ public class CoachRequest {
     }
 
 }
-=======
-package com.example.foooball_app.dto.request;
-
-import java.sql.Date;
-
-public class CoachRequest {
-    private String coachName;
-
-    private String country;
-
-    private Date dateOfBirth;
-
-//    private  Team team;
-
-    private Integer yearsOfExperience;
-
-    // Getters and setters
-
-    public String getCoachName() {
-        return coachName;
-    }
-
-    public void setCoạchName(String coachName) {
-        this.coachName = coachName;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-//    public Team getTeam() {
-//        return team;
-//    }
-//
-//    public void setTeam(Team team) {
-//        this.team = team;
-//    }
-
-    public Integer getYearsOfExperience() {
-        return yearsOfExperience;
-    }
-
-    public void setYearsOfExperience(Integer yearsOfExperience) {
-        this.yearsOfExperience = yearsOfExperience;
-    }
-
-}
->>>>>>> af9d94641fd062ce08cf4020196d90695601d54c
